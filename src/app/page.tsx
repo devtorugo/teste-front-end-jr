ae200dfbfb169164543260d84341628447dcbe78
devtorugo <victorzarbesu@gmail.com>
2024-06-07 02:41:02 -0300

    Importando Banner na page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import Banner from "./components/Banner";
 
 
+
 export default function Home() {
   return (
 
     <main>
-        <Banner />
+     <Banner/>
     </main>
   );
 }
